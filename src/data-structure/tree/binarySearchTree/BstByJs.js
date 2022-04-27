89f84811b9ebb65b778af2cd0afcbef9acc89fa2
wangchen <wangchen130166@gmail.com>
2022-04-27 15:06:46 +0800

    add: 二叉搜索树bst删除节点功能的实现。

--- src/data-structure/tree/binarySearchTree/BstByJs.js
+++ src/data-structure/tree/binarySearchTree/BstByJs.js
@@ -114,7 +114,77 @@ function BstByJs() {
    * @param val: 节点值
    */
   BstByJs.prototype.remove = function (val) {
-
+    // 删除节点的父节点
+    let parent = null
+    // 当前节点，即要删除的节点
+    let current = this.root
+    // 删除节点是否是父节点的左子节点
+    let isLeft = true
+    while (val !== current.val) {
+      parent = current
+      if (val < current.val) {
+        current = current.left
+        isLeft = true
+      } else {
+        current = current.right
+        isLeft = false
+      }
+      if (!current) return false
+    }
+    // 删除节点为叶子节点
+    if (!current.left && !current.right) {
+      if (current == this.root) {
+        this.root = null
+      } else if (isLeft) {
+        parent.left = null
+      } else {
+        parent.right = null
+      }
+    } else if (!current.right) { // 删除节点只有一个左子节点
+      if (current == this.root) {
+        this.root = current.left
+      } else if (isLeft) {
+        parent.left = current.left
+      } else {
+        parent.right = current.left
+      }
+    } else if (!current.left) { // 删除节点只有一个右子节点
+      if (current == this.root) {
+        this.root = current.right
+      } else if (isLeft) {
+        parent.left = current.right
+      } else {
+        parent.right = current.right
+      }
+    } else {
+      let successorNode = this.getSuccessor(current)
+      if (current == this.root) {
+        this.root = successorNode
+      } else if (isLeft) {
+        parent.left = successorNode
+      } else {
+        parent.right = successorNode
+      }
+      successorNode.left = current.left
+    }
+    return current
+  }
+  /**
+   * 获取后继节点，返回的后继节点为删除节点的右子树
+   * @param node： 删除节点
+   */
+  BstByJs.prototype.getSuccessor = function (node) {
+    let parent = node
+    let current = node.right
+    while (current.left) {
+      parent = current
+      current = current.left
+    }
+    if (parent != node) {
+      parent.left = current.right
+      current.right = node.right
+    }
+    return current
   }
 }
 
@@ -137,18 +207,54 @@ bst.insert(18)
 bst.insert(25)
 bst.insert(19)
 bst.insert(6)
-console.log(bst);
-bst.preOrderTraverse(function (node) {
-  console.log(node.val);
-})
-console.log('------------------------');
+// console.log(bst);
+// bst.preOrderTraverse(function (node) {
+//   console.log(node.val);
+// })
+// console.log('------------------------');
+// bst.inOrderTraverse(function (node) {
+//   console.log(node.val);
+// })
+// console.log('------------------------');
+// bst.postOrderTraverse(function (node) {
+//   console.log(node.val);
+// })
+// console.log('------------------------');
+// console.log(bst.min());
+// console.log(bst.max());
+
 bst.inOrderTraverse(function (node) {
   console.log(node.val);
 })
 console.log('------------------------');
-bst.postOrderTraverse(function (node) {
+console.log(bst.remove(6));
+bst.inOrderTraverse(function (node) {
   console.log(node.val);
 })
 console.log('------------------------');
-console.log(bst.min());
-console.log(bst.max());
+console.log(bst.remove(11));
+bst.inOrderTraverse(function (node) {
+  console.log(node.val);
+})
+console.log('------------------------');
+console.log(bst.remove(15));
+bst.inOrderTraverse(function (node) {
+  console.log(node.val);
+})
+console.log('------------------------');
+
+console.log(bst.remove(19));
+bst.inOrderTraverse(function (node) {
+  console.log(node.val);
+})
+console.log('------------------------');
+console.log(bst.remove(25));
+bst.inOrderTraverse(function (node) {
+  console.log(node.val);
+})
+console.log('------------------------');
+console.log(bst.remove(11));
+bst.inOrderTraverse(function (node) {
+  console.log(node.val);
+})
+console.log('------------------------');
