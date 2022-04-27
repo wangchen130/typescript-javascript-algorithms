3984758aa2cdf0b11c1293e4e623117f35383c83
wangchen <wangchen130166@gmail.com>
2022-04-27 17:06:51 +0800

    add: 使用es5实现冒泡排序。

---
+++ src/algorithm/sortAlgorithms/js/ArrayList.js
@@ -0,0 +1,70 @@
+function ArrayList() {
+  this.array = []
+  // insert(item): 插入元素
+  // insertList(itemList): 批量插入元素
+  // swap(m,n): 交换m位置和n位置的两个元素
+  // bubbleSort(): 冒泡排序
+  // selectSort(): 选择排序
+  // insertSort(): 插入排序
+  // shellSort(): 希尔排序
+  // quickSort(): 快速排序
+  // toString(): 重写toString方法
+  /**
+   * insert(item): 插入元素
+   * @param item: 需要插入的元素
+   */
+  ArrayList.prototype.insert = function (item) {
+    this.array.push(item)
+  }
+  /**
+   * insertList(itemList): 批量插入元素
+   * @param itemList： 元素数组
+   */
+  ArrayList.prototype.insertList = function (itemList) {
+    for (var i = 0; i < itemList.length; i++) {
+      var item = itemList[i]
+      this.array.push(item)
+    }
+  }
+  /**
+   * swap(m,n): 交换m位置和n位置的两个元素
+   * @param m
+   * @param n
+   */
+  ArrayList.prototype.swap = function (m, n) {
+    var temp = this.array[m]
+    this.array[m] = this.array[n]
+    this.array[n] = temp
+  }
+  /**
+   * 冒泡排序
+   */
+  ArrayList.prototype.bubbleSort = function () {
+    var len = this.array.length
+    for (var i = len - 1; i >= 0 ; i--) {
+      for (var j = 0; j < i; j++) {
+        if (this.array[j] > this.array[j + 1]) {
+          this.swap(j,j + 1)
+        }
+      }
+    }
+  }
+  ArrayList.prototype.selectSort = function () {
+    
+  }
+  ArrayList.prototype.insertSort = function () {
+    
+  }
+  /**
+   * 重写toString方法
+   * @returns {string}
+   */
+  ArrayList.prototype.toString = function () {
+    return this.array.join('-')
+  }
+}
+var arrayList = new ArrayList()
+arrayList.insertList([100,1,50,40,90,80,20,10,30,70,60])
+console.log(arrayList.toString());
+arrayList.bubbleSort()
+console.log(arrayList.toString());
